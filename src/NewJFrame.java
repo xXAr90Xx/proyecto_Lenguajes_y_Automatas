54764e22e36190eaf4c8bd00238b54afea08ad0f
Arath <105472088+xXAr90Xx@users.noreply.github.com>
2023-11-11 19:43:39 -0600

    Modificacion a AnalizadorLexico en las ER

--- src/NewJFrame.java
+++ src/NewJFrame.java
@@ -1,6 +1,10 @@
 
 import java.awt.Color;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -9,12 +13,13 @@ import java.io.IOException;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Kevin Alberto Perez
  */
 public class NewJFrame extends javax.swing.JFrame {
+
+    public int cuenta = 0;
 
     /**
      * Creates new form NewJFrame
@@ -33,16 +38,23 @@ public class NewJFrame extends javax.swing.JFrame {
     private void initComponents() {
 
         jScrollPane1 = new javax.swing.JScrollPane();
-        jTextArea1 = new javax.swing.JTextArea();
+        jTextAreaCodigo = new javax.swing.JTextArea();
         jScrollPane2 = new javax.swing.JScrollPane();
         jTextOutput = new javax.swing.JTextArea();
         btnGualdal = new javax.swing.JButton();
+        BtnAnal = new javax.swing.JButton();
+        jTextAreaLinea = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jTextArea1.setColumns(20);
-        jTextArea1.setRows(5);
-        jScrollPane1.setViewportView(jTextArea1);
+        jTextAreaCodigo.setColumns(20);
+        jTextAreaCodigo.setRows(5);
+        jTextAreaCodigo.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyTyped(java.awt.event.KeyEvent evt) {
+                jTextAreaCodigoKeyTyped(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jTextAreaCodigo);
 
         jTextOutput.setColumns(20);
         jTextOutput.setRows(5);
@@ -57,41 +69,107 @@ public class NewJFrame extends javax.swing.JFrame {
             }
         });
 
+        BtnAnal.setText("Leer y Analizar");
+        BtnAnal.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                BtnAnalMouseClicked(evt);
+            }
+        });
+
+        jTextAreaLinea.setColumns(20);
+        jTextAreaLinea.setRows(5);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 565, Short.MAX_VALUE)
-            .addComponent(jScrollPane2)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGap(53, 53, 53)
                 .addComponent(btnGualdal, javax.swing.GroupLayout.PREFERRED_SIZE, 86, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(BtnAnal, javax.swing.GroupLayout.PREFERRED_SIZE, 127, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(37, 37, 37))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jTextAreaLinea, javax.swing.GroupLayout.DEFAULT_SIZE, 16, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 545, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jScrollPane2)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(16, Short.MAX_VALUE)
-                .addComponent(btnGualdal)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 310, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnGualdal)
+                    .addComponent(BtnAnal))
+                .addGap(54, 54, 54)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jTextAreaLinea)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 310, Short.MAX_VALUE))
                 .addGap(18, 18, 18)
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 108, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnGualdalActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGualdalActionPerformed
-        System.out.println("PUTAAAAAAAAAAS");   
+        System.out.println("Gualdado");
         try (BufferedWriter writer = new BufferedWriter(new FileWriter("Codigo"))) {
-            writer.write(jTextArea1.getText());
+            writer.write(jTextAreaCodigo.getText());
             System.out.println("File saved successfully.");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }//GEN-LAST:event_btnGualdalActionPerformed
+
+    private void BtnAnalMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BtnAnalMouseClicked
+        leerArchivo("Codigo");
+    }//GEN-LAST:event_BtnAnalMouseClicked
+
+    private void jTextAreaCodigoKeyTyped(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_jTextAreaCodigoKeyTyped
+        // TODO add your handling code here:
+         char keyChar = evt.getKeyChar();
+                    System.out.println("Key Typed: " + keyChar);
+
+                    if (keyChar == '\n') {
+                        System.out.println("Enter.");
+
+                        // Incrementar el contador solo si la última línea no está vacía
+                        String[] lineas = jTextAreaCodigo.getText().split("\\n");
+                        if (lineas.length > 0 && !lineas[lineas.length - 1].trim().isEmpty()) {
+                            cuenta++;
+
+                            // Agregar el número al JTextArea de números
+                            jTextAreaLinea.setText("");
+                            for (int i = 1; i <= cuenta; i++) {
+                                jTextAreaLinea.append(i + "\n");
+                            }
+                        }
+                    }
+    }//GEN-LAST:event_jTextAreaCodigoKeyTyped
+
+    public static String leerArchivo(String nombreArchivo) {
+        StringBuilder contenido = new StringBuilder();
+        try (BufferedReader reader = new BufferedReader(new FileReader(nombreArchivo))) {
+            String linea;
+
+            while ((linea = reader.readLine()) != null) {
+                contenido.append(linea).append("\n");
+
+            }
+            System.out.println("File read successfully.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return contenido.toString();
+    }
 
     /**
      * @param args the command line arguments
@@ -107,16 +185,24 @@ public class NewJFrame extends javax.swing.JFrame {
                 if ("Nimbus".equals(info.getName())) {
                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
                     break;
+
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrame.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrame.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrame.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
+
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(NewJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(NewJFrame.class
+                    .getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
@@ -129,10 +215,12 @@ public class NewJFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BtnAnal;
     private javax.swing.JButton btnGualdal;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextAreaCodigo;
+    public static javax.swing.JTextArea jTextAreaLinea;
     private javax.swing.JTextArea jTextOutput;
     // End of variables declaration//GEN-END:variables
 }
